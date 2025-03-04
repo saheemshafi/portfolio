0f878195c30046e9b83840269921b8b42bb33925
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2025-03-04 06:16:44 +0530

    feat: use supabase edge functions for means of sending contact mails

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,35 +1,30 @@
 import supabase from "@/lib/supabase/supabase";
 import { contactFormSchema } from "@/lib/zod/contactFormSchema";
-import { env } from "@/lib/zod/envSchema";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-import nodemailer from "nodemailer";
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "MAILID@gmail.com",
-    pass: "YOUR PASSWORD",
-  },
-});
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const contactInfo = contactFormSchema.parse(body);
 
-    await supabase
+    const contact = await supabase
       .from("contacts")
       .insert({
-        from: contactInfo.name,
+        sender: contactInfo.name,
         email: contactInfo.email,
         message: contactInfo.message,
-        email_sent: true,
       })
+      .select()
       .throwOnError();
+
+    const response = await supabase.functions.invoke("send-email", {
+      body: contact.data?.[0] || {},
+    });
+
+    if (response.error) {
+      throw response.error;
+    }
 
     return NextResponse.json(
       { success: true, message: `Message Sent!` },
